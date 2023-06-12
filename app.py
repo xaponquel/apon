85a8c49ad151f6070e742ba160c4589fe15036ed
Xaponquel <130245408+xaponquel@users.noreply.github.com>
2023-06-12 12:57:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,104 +1,61 @@
+import pickle 
 import streamlit as st
 import pandas as pd
-import plotly_express as px
 
+# Membaca model
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-st.set_page_config(layout='wide')
-st.header('Blog Stats Dashboard')
+# Membaca data dari file CSV
+data = pd.read_csv('data.csv')
 
+# Judul Web
+st.title('Data Mining Prediksi Diabetes by Abror')
 
-#Awal Support Function
-def calculate_differences(df): #untuk menghitung selisih
-    #menghitung selisih current month and previous month untuk kolom followers,email_subs,referred_members
-    #df['nama_kolom'].diff() menghitung selisih dan membuat kolom baru untuk menyimpan selisihnya 
-    df['followers_increase'] = df['followers'].diff()
-    df['email_subs_increase'] = df['email_subs'].diff()
-    df['referred_members_increase'] = df['referred_members'].diff()
-    return df
+# Membagi kolom
+col1, col2 = st.columns(2)
 
-#untuk mencari nilai tiap kolom pada baris terakhir
-def get_latest_monthly_metrics(df):
+with col1:
+    st.markdown('<style>div.stTextInput.st-cc input {border: 1px solid black;}</style>', unsafe_allow_html=True)
+    Pregnancies = st.text_input('Masukkan bulan kehamilan')
 
-    df['date'] = pd.to_datetime(df['date']) #mengubah text menjadi tanggal beneran
-    df['month'] = df['date'].dt.month_name() #mengambil nama bulan
-    
-    latest_month = df['month'].iloc[-1] #mengambil nilai month di baris terakhir
-    
-    
-    latest_follower_count = [ 
-                            df['followers'].iloc[-1], #mencari nilai follower pada baris terakhir
-                            df['followers_increase'].iloc[-1] #mencari nilai perubahan follower pada baris terakhir
-                             ]
-    
-    latest_sub_count = [
-                        df['email_subs'].iloc[-1], #mencari nilai subscriber pada baris terakhir
-                        df['email_subs_increase'].iloc[-1] #mencari nilai perubahan subscriber pada baris terakhir
-                        ]
-    
-    latest_refs_count = [
-                        df['referred_members'].iloc[-1], #mencari nilai referral pada baris terakhir 
-                        df['referred_members_increase'].iloc[-1] #mencari nilai perubahan referral pada baris terakhir
-                        ]
+with col2:
+    st.markdown('<style>div.stTextInput.st-cc input {border: 1px solid black;}</style>', unsafe_allow_html=True)
+    Glucose = st.text_input('Masukkan tingkat kadar gula')
 
-    return latest_month, latest_follower_count, latest_sub_count, latest_refs_count
+with col1:
+    st.markdown('<style>div.stTextInput.st-cc input {border: 1px solid black;}</style>', unsafe_allow_html=True)
+    BloodPressure = st.text_input('Masukkan tingkat tekanan darah')
 
-#untuk updata data csv
-def update_stats_csv(date, followers, email_subs, ref_members):
-    with open('data.csv', 'a+') as f:
-        f.write(f'\n{date},{followers},{email_subs},{ref_members}')
-#Akhir Support Function
+with col2:
+    st.markdown('<style>div.stTextInput.st-cc input {border: 1px solid black;}</style>', unsafe_allow_html=True)
+    SkinThickness = st.text_input('Masukkan ketebalan kulit')
 
+with col1:
+    st.markdown('<style>div.stTextInput.st-cc input {border: 1px solid black;}</style>', unsafe_allow_html=True)
+    Insulin = st.text_input('Masukkan tingkat Insulin')
 
-#Mulai Sidebar
-with st.sidebar.form('Enter the latest stats from your Medium dashboard'):
+with col2:
+    st.markdown('<style>div.stTextInput.st-cc input {border: 1px solid black;}</style>', unsafe_allow_html=True)
+    BMI = st.text_input('Masukkan nilai BMI')
 
-    date = st.date_input('Date')
-    
-    follower_num = st.number_input('Followers', step=1)
-    email_subs = st.number_input('Email Subscribers', step=1)
-    ref_members = st.number_input('Referred Members', step=1)
+with col1:
+    st.markdown('<style>div.stTextInput.st-cc input {border: 1px solid black;}</style>', unsafe_allow_html=True)
+    DiabetesPedigreeFunction = st.text_input('Masukkan total keluarga yang memiliki riwayat diabetes')
 
-    submit_stats = st.form_submit_button('Write Stats to CSV')
+with col2:
+    st.markdown('<style>div.stTextInput.st-cc input {border: 1px solid black;}</style>', unsafe_allow_html=True)
+    Age = st.text_input('Masukkan umur Anda')
 
-    if submit_stats:
-        #kalau button diklik, kodingan ini akan dijalankan
-        update_stats_csv(date, follower_num, email_subs, ref_members)
-        st.info(f'Stats for {date} have been added to the csv file')
-#Akhir Sidebar
+# Kode untuk prediksi
+diab_diagnosis = ''
 
+# Tombol prediksi
+if st.button('Test Prediksi Diabetes'):
+    diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
 
-#Awal halaman utama
-df = pd.read_csv('data.csv')   #untuk membaca file data
-df = calculate_differences(df) #untuk mendapatkan selisih
+    if diab_prediction[0] == 1:
+        diab_diagnosis = 'Pasien terkena penyakit Diabetes'
+    else:
+        diab_diagnosis = 'Pasien tidak terkena penyakit Diabetes'
 
-#untuk menampilkan dataframe
-with st.expander('View Raw Data'):
-    st.dataframe(df)
-
-#untuk mencari metrik data terakhir
-month, followers, subs, refs = get_latest_monthly_metrics(df)
-st.write(f'### Blog Stats for {month}')
-
-#untuk menampilkan metrik
-col1, col2, col3 = st.columns(3)
-col1.metric('Followers', followers[0], delta=round(followers[1])) #metrik follower
-col2.metric('Email Subscribers', subs[0], delta=round(subs[1])) #metrik subscriber
-col3.metric('Referred Members', refs[0], delta=round(refs[1])) #metrik member
-
-
-#untuk menampilkan grafik
-#grafik pertama
-fig_followers = px.line(df, x=df.date, y='followers', title='Followers')
-st.plotly_chart(fig_followers, use_container_width=True)
-#grafik pertama
-
-#grafik kedua
-plot_col1, plot_col2 = st.columns(2)
-
-fig_subscribers = px.bar(df, x=df.date, y='email_subs', title='Email Subscribers')
-plot_col1.plotly_chart(fig_subscribers, use_container_width=True)
-
-fig_subscribers = px.bar(df, x=df.date, y='referred_members', title='Referred Members')
-plot_col2.plotly_chart(fig_subscribers, use_container_width=True)
-#grafik kedua
-#Akhir halaman utama
+    st.success(diab_diagnosis)
